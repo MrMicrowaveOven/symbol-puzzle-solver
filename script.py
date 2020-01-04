4f39757242cb1264876aa69828078a4f22176446
Benjamin Zagorski <benj@zagorski.com>
2020-01-03 23:38:50 -0800

    Solution

---
+++ script.py
@@ -0,0 +1,27 @@
+import random
+
+LEFT_SIDE = [52,4,6,5]
+RIGHT_SIDE = 40
+
+def get_symbols(left_side, right_side):
+    solved = False
+    SYMBOLS = ['+', '-', '/', '*']
+    while not solved:
+        goal_solution = left_side[0]
+        symbols = SYMBOLS
+        random.shuffle(symbols)
+        symbols = symbols[0:3]
+        for i, symbol in enumerate(symbols):
+            if symbol == '+':
+                goal_solution += left_side[i + 1]
+            if symbol == '-':
+                goal_solution -= left_side[i + 1]
+            if symbol == '/':
+                goal_solution /= left_side[i + 1]
+            if symbol == '*':
+                goal_solution *= left_side[i + 1]
+        if goal_solution == right_side:
+            solved = True
+    print symbols
+
+get_symbols(LEFT_SIDE, RIGHT_SIDE)
